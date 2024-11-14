130bc15d824ba07113aaf24bafa7d0eec7e9cf8c
AstonisherVR <152096497+AstonisherVR@users.noreply.github.com>
2024-11-14 10:34:07 +0200

    Main Brach Bases
    I want to make some base stuff for the main branch, so when I witch branches to have all theese stuff togheter.

--- Player/player.gd
+++ Player/player.gd
@@ -1,27 +1,30 @@
 extends Node3D
 
 @export var degrees_to_rotate := 90
-
+var is_turning := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	handle_input(delta)
+	handle_input()
 
-func handle_input(delta):
-	if Input.is_action_just_pressed("LEFT"):
+func handle_input():
+	if Input.is_action_just_pressed("LEFT") and not is_turning:
+		is_turning = true
 		#rotation_degrees.y += degrees_to_rotate
 		for i in range(45):
 			rotation_degrees.y += 2
-			await get_tree().create_timer(delta).timeout
-
-	if Input.is_action_just_pressed("RIGHT"):
+			await get_tree().create_timer(0.01).timeout
+		is_turning = false
+	elif Input.is_action_just_pressed("RIGHT") and not is_turning:
 		#rotation_degrees.y -= degrees_to_rotate
+		is_turning = true
 		for i in range(45):
 			rotation_degrees.y -= 2
-			await get_tree().create_timer(delta).timeout
+			await get_tree().create_timer(0.01).timeout
+		is_turning = false
 
 func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("L_CLICK"):
