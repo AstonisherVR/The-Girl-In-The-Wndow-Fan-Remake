11170b6dba5e8115256437a71375b25b2963389c
Astonisher <astonisherred@gmail.com>
2024-11-14 16:00:55 +0200

    Faster Player turn & New Player Sprite

--- Player/player.gd
+++ Player/player.gd
@@ -1,30 +1,25 @@
 extends Node3D
 
-@export var degrees_to_rotate := 90
 var is_turning := false
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready() -> void:
+	pass
+
 func _process(delta: float) -> void:
 	handle_input()
 
 func handle_input():
 	if Input.is_action_just_pressed("LEFT") and not is_turning:
-		is_turning = true
-		#rotation_degrees.y += degrees_to_rotate
-		for i in range(45):
-			rotation_degrees.y += 2
-			await get_tree().create_timer(0.01).timeout
-		is_turning = false
+		rotate_player(9)
 	elif Input.is_action_just_pressed("RIGHT") and not is_turning:
-		#rotation_degrees.y -= degrees_to_rotate
-		is_turning = true
-		for i in range(45):
-			rotation_degrees.y -= 2
-			await get_tree().create_timer(0.01).timeout
-		is_turning = false
+		rotate_player(-9)
+
+func rotate_player(rotation_speed: float):
+	is_turning = true
+	for i in range(10):
+		rotation_degrees.y += rotation_speed
+		await get_tree().create_timer(0.001).timeout
+	is_turning = false
 
 func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("L_CLICK"):
