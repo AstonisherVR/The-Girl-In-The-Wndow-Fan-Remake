4af31a84bf7601e99dad7f3891d6a4810f9f9d32
Astonisher <astonisherred@gmail.com>
2024-12-05 13:26:29 +0200

    Base

--- Autoloads/Global.gd
+++ Autoloads/Global.gd
@@ -1,8 +1,1 @@
 extends Node
-
-func _ready() -> void:
-	pass
-
-
-func _process(delta: float) -> void:
-	pass
--- Player/player.gd
+++ Player/player.gd
@@ -8,20 +8,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	handle_input()
 
-func handle_input():
+func handle_input() -> void:
 	if Input.is_action_just_pressed("LEFT") and not is_turning:
 		rotate_player(9)
 	elif Input.is_action_just_pressed("RIGHT") and not is_turning:
 		rotate_player(-9)
 
-func rotate_player(rotation_speed: float):
+func rotate_player(rotation_speed: float) -> void:
 	is_turning = true
 	for i in range(10):
 		rotation_degrees.y += rotation_speed
 		await get_tree().create_timer(0.001).timeout
 	is_turning = false
 
+# TEST Placeholder code
 func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("L_CLICK"):
-		$"../House Prototype/Pickable Object".position.y += 0.25 #This is test for the pickable object.
+		var object: Node3D = $"../House Prototype/Pickable Object"
+		object.global_position.y += 0.25 #This is test for the pickable object.
 		print("Ouch! I am an interactable Pickable object and you just touched me!") #Just some random text to test if it works correctly.
